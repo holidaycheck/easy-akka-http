eb4e71bdef5abf0d828a3e523283df29e6227197
Yannick Gladow <yannick.gladow@holidaycheck.com>
2019-08-05 17:16:16 +0200

    Add NonEmptyList Unmarshaller

---
+++ easy-akka-marshalling/src/main/scala/com/holidaycheck/akka/http/NelUnmarshaller.scala
@@ -0,0 +1,15 @@
+package com.holidaycheck.akka.http
+
+import akka.http.scaladsl.unmarshalling.Unmarshaller.CsvSeq
+import akka.http.scaladsl.unmarshalling.{FromStringUnmarshaller, Unmarshaller}
+import cats.data.NonEmptyList
+import cats.syntax.list._
+
+object NelUnmarshaller {
+  implicit def nelUnmarshaller[A: FromStringUnmarshaller]: Unmarshaller[String, NonEmptyList[A]] =
+    Unmarshaller
+      .strict[String, String](s =>
+        if (s.nonEmpty) s else throw new IllegalArgumentException(s"$s needs to be non-empty"))
+      .andThen(CsvSeq[A].map(seq =>
+        seq.toList.toNel.getOrElse(throw new IllegalArgumentException(s"$seq is not an non-empty list"))))
+}
