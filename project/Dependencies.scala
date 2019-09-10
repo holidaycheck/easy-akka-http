37df42e60bf416f18821238a46a51610fa9a97a7
Scala Steward <me@scala-steward.org>
2019-09-10 18:25:08 +0200

    Update cats-effect to 2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val ScalaTestVersion     = "3.0.8"
   val AkkaHttpVersion      = "10.1.9"
-  val CatsEffectVersion    = "1.4.0"
+  val CatsEffectVersion    = "2.0.0"
   val AkkaVersion          = "2.5.25"
   val CirceVersion         = "0.11.1"
   val AkkaHttpCirceVersion = "1.27.0"
