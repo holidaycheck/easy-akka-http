08a5c4e8f68174c0d7228339bfd4d8f0f40765e6
Scala Steward <me@scala-steward.org>
2020-02-14 20:17:21 +0100

    Update akka-http-circe to 1.31.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val CatsEffectVersion    = "2.0.0"
   val AkkaVersion          = "2.6.2"
   val CirceVersion         = "0.13.0"
-  val AkkaHttpCirceVersion = "1.30.0"
+  val AkkaHttpCirceVersion = "1.31.0"
   val RefinedVersion       = "0.9.12"
 
   lazy val circe = Seq(
