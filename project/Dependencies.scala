72872fa78d9c10b86c25b25bd78551afe608dbba
Scala Steward <scala_steward@virtuslab.com>
2022-09-19 17:28:02 +0000

    Update circe-core, circe-generic, ... to 0.14.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val AkkaHttpVersion      = "10.1.11"
   val CatsEffectVersion    = "2.1.2"
   val AkkaVersion          = "2.6.2"
-  val CirceVersion         = "0.13.0"
+  val CirceVersion         = "0.14.3"
   val AkkaHttpCirceVersion = "1.30.0"
   val RefinedVersion       = "0.9.12"
 
